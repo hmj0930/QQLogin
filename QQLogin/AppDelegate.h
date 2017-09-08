767186398071a61cf2c5811f5737ac4494d6fd6c
HanMJ <915660592@qq.com>
2017-09-08 08:10:04 +0800

    Initial Commit

---
+++ QQLogin/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  QQLogin
+//
+//  Created by MJ on 2017/9/8.
+//  Copyright © 2017年 韩明静. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ QQLogin/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  QQLogin
+//
+//  Created by MJ on 2017/9/8.
+//  Copyright © 2017年 韩明静. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
